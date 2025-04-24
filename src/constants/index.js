1158315717a80de746c0a358bd6d4b0ca3f39a19
sachin455 <karkisachin455@gmail.com>
2025-04-24 20:23:23 +0545

    added link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
+import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `At Hamro Tech, we’re not just developers—we're problem-solvers, creators, and collaborators. With a global team and a passion for innovation, we bring your vision to life through beautifully built, reliable, and scalable tech solutions. From startups to enterprises, we help businesses innovate, grow, and thrive through powerful technology and thoughtful design.`;
@@ -49,9 +49,9 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    githubLink: "https://github.com",
+    githubLink: "https://ecommerce-nu-olive-37.vercel.app/",
     livelink: "https:google.com",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "Nextjs"],
   },
   {
     id:2,
@@ -65,11 +65,11 @@ export const PROJECTS = [
   },
   {
     id:3,
-    name: "Portfolio Website",
+    name: "CMovies",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    githubLink: "https://github.com",
+      "A website like Netflix where users can browse movies and add it to their favorite Collection",
+    githubLink: "https://cmovies-sachin455s-projects.vercel.app/",
     livelink: "https:google.com",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
